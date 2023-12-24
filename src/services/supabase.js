64bb1078ba5ac6c0a7f9d513c796818193a95ab9
Abdul Wahed Sabury <sabury2020@gmail.com>
2023-12-24 13:23:56 +0200

    supabase added to project

---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://pawqryafgeunwnokqkhs.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhd3FyeWFmZ2V1bndub2txa2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM0MTE4MTMsImV4cCI6MjAxODk4NzgxM30.IEE6Fq_vbjagQyWGvxlr1j7RTGD7kB5M2WE5RGbShN8";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
