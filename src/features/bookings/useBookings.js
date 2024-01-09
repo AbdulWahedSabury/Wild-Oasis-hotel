95638fab391f3593db2667aec1f64d1126b39394
Abdul Wahed Sabury <sabury2020@gmail.com>
2024-01-09 15:37:05 +0200

    prefetching data for pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,7 +1,12 @@
 import { useSearchParams } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+
 import { getBookings } from "../../services/apiBookings";
 import { useQuery } from "@tanstack/react-query";
+import { PER_PAGE } from "../../utils/constants";
 export function useBookings({ filter, sortBy }) {
+  const queryClient = useQueryClient();
+
   const [searchParams] = useSearchParams();
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
 
@@ -13,6 +18,17 @@ export function useBookings({ filter, sortBy }) {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
-
+  // PreFetching Data
+  const pageCount = Math.ceil(count / PER_PAGE);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
   return { isLoading, bookings, error, count };
 }
