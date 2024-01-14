83844128cfa16d4030e7ecab4a0ce1f05d274a42
Abdul Wahed Sabury <sabury2020@gmail.com>
2024-01-14 13:33:26 +0200

    [styles] adding dark mode to app

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools} from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
 import ProtectedRoutes from "./ui/ProtectedRoutes";
 import Account from "./pages/Account";
+import { DarkModeProvider } from "./context/DarkModeContext";
 function App() {
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -24,42 +25,55 @@ function App() {
     },
   });
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen = {false} />
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<ProtectedRoutes><AppLayout /></ProtectedRoutes>}>
-            <Route index element={<Navigate replace to="dashboard" />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="booking/:bookingId" element={<Booking />} />
-            <Route path="checkIn/:bookingId" element={<Checkin />} />
-            <Route path="cabins" element={<Cabins />} />
-            <Route path="users" element={<Users />} />
-            <Route path="account" element={<Account />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster position='top-center' containerStyle={{margin : "8px"}} gutter={12} toastOptions={{
-        success : {
-          duration : 3000
-        },
-        error : {
-          duration : 5000
-        },
-        style : {
-          padding : '16px 24px',
-          fontSize : '16px',
-          backgroundColor : "var(---color-grey-0)",
-          color : "var(---color-grey-700)",
-          maxWidth : "500px"
-        }
-      }} />
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              element={
+                <ProtectedRoutes>
+                  <AppLayout />
+                </ProtectedRoutes>
+              }
+            >
+              <Route index element={<Navigate replace to="dashboard" />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="bookings" element={<Bookings />} />
+              <Route path="booking/:bookingId" element={<Booking />} />
+              <Route path="checkIn/:bookingId" element={<Checkin />} />
+              <Route path="cabins" element={<Cabins />} />
+              <Route path="users" element={<Users />} />
+              <Route path="account" element={<Account />} />
+              <Route path="settings" element={<Settings />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster
+          position="top-center"
+          containerStyle={{ margin: "8px" }}
+          gutter={12}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              padding: "16px 24px",
+              fontSize: "16px",
+              backgroundColor: "var(---color-grey-0)",
+              color: "var(---color-grey-700)",
+              maxWidth: "500px",
+            },
+          }}
+        />
       </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,25 +1,29 @@
-import styled from "styled-components"
-import Logout from "../features/authentication/Logout"
-import { HiOutlineUser } from "react-icons/hi2"
-import { useNavigate } from "react-router-dom"
-import ButtonIcon from "./ButtonIcon"
+import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
+import { HiOutlineUser } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+import ButtonIcon from "./ButtonIcon";
+import DarkModeToggle from "./DarkModeToggle";
 
 const StyleHeaderMenu = styled.ul`
-    display: flex;
-    gap: 0.4rem;
-`
-export default function HeaderMenu(){
-    const navigate = useNavigate();
-    return(
-        <StyleHeaderMenu>
-            <li>
-                <ButtonIcon onClick={()=>navigate('/account')}>
-                    <HiOutlineUser />
-                </ButtonIcon>
-            </li>
-            <li>
-                <Logout />
-            </li>
-        </StyleHeaderMenu>
-    )
+  display: flex;
+  gap: 0.4rem;
+`;
+export default function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyleHeaderMenu>
+      <li>
+        <Logout />
+      </li>
+      <li>
+        <DarkModeToggle />
+      </li>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+    </StyleHeaderMenu>
+  );
 }
